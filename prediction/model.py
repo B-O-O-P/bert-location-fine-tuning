208bdedde813857734c5ba17f47d5d709b735d0a
Chizhikov Dmitriy <dimkanut@gmail.com>
2021-04-22 17:15:57 +0300

    fix scalar product

--- prediction/model.py
+++ prediction/model.py
@@ -1,7 +1,5 @@
-import numpy as np
 from transformers import BertModel
-
-from torch import nn
+from torch import bmm, nn
 
 class BERT(nn.Module):
     def __init__(self, embedding=None):
@@ -11,18 +9,18 @@ class BERT(nn.Module):
         # Эмбеддинг
         self.embedding = embedding
         # Линейный слой
-        self.linear = nn.Linear(768, 1)
+        self.linear = nn.Linear(768, 256)
 
     def forward(self, input_ids, input_background, input_mask=None, token_type_ids=None):
         # Получение cls токена
         output = self.model(input_ids=input_ids, attention_mask=input_mask, token_type_ids=token_type_ids)
         output = output['pooler_output']
         # Применение линейного слоя
-        output = self.linear(output)
+        output = self.linear(output).unsqueeze(1)
 
         # Получение эмбеддинга для бэкграунда
-        background = self.embedding(input_background)
+        background = self.embedding(input_background).unsqueeze(2)
 
         # Скалярное произведение
-        y_pred = np.tensordot(output, background)
+        y_pred = bmm(output, background)
         return y_pred
